8f5e79cb3c212fe2d356085fabce7f422c9b5f62
rafi0012 <rafiultalukdar007@gmail.com>
2026-05-20 15:00:43 +0600

    Add bio.java & assg 01

---
+++ bio.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Rafiul";
+        String lastName = "Talukdar";
+        int age = 22;
+
+        System.out.println(firstName + " " + lastName + " Age " + age);
+    }
+}
