afcdec20a782954539fc5cd3150a992e480cf045
rafi0012 <rafiultalukdar007@gmail.com>
2026-05-21 17:18:00 +0600

    explain simple condition

---
+++ simple-condition.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are Adult");
+        }
+        else {
+            System.out.println("You are Minor");
+        }
+
+    }
+}
