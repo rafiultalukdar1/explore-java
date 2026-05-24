706e32af67f1d8d0450593ead0bec608a4f506e7
rafi0012 <rafiultalukdar007@gmail.com>
2026-05-24 21:44:15 +0600

    explore variable

---
+++ method.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void greet() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        greet();
+        greet();
+        greet();
+        greet();
+    }
+}
---
+++ variable.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args){
+        String name = "Rafi";
+        int age = 22;
+        double cgpa = 3.20;
+        boolean pass = true;
+
+        System.out.println("Name: " + name + "," + " Age: " + age + "," + " CGPA: " + cgpa + "," + " Pass: " + pass);
+    }
+
+}
