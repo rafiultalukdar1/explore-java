552804811ffe4a49fd1fa4ccfa92660002d4166f
rafi0012 <rafiultalukdar007@gmail.com>
2026-06-01 10:34:16 +0600

    explore oop_step06

---
+++ oop_step06.java
@@ -0,0 +1,17 @@
+public class oop_step06 {
+
+    public static void main(String[] args) {
+
+        try {
+            int result = 10 / 2;
+
+            System.out.println(result);
+
+        } catch (Exception e) {
+
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        System.out.println("Program continues...");
+    }
+}
